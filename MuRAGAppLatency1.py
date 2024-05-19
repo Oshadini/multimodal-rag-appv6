8e6ee4fbedaee4524b0a4b24ecec6a03e24d58aa
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-20 02:51:16 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -303,11 +303,13 @@ if uploaded_file is not None:
 
         st.session_state["img_base64_list"] = img_base64_list
         st.session_state["image_summaries"] = image_summaries
+        st.write(image_summaries)
         st.write(f"{bullet_point} \t\tImage summaries generation completed")
 
     else:
         img_base64_list = st.session_state["img_base64_list"]  
         image_summaries = st.session_state["image_summaries"]  
+        st.write(image_summaries)
     
 
 
